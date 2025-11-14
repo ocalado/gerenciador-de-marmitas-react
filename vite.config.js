3b2ce16ea9b1aa6e93f01567edcf5aa8b0ce572e
Europa <t_lcalado@tenda.com>
2025-11-14 10:36:42 -0300

    refactor: ajusta configurações do projeto

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gerenciador-de-marmitas-react/',
   plugins: [
     react(),
     tailwindcss()
